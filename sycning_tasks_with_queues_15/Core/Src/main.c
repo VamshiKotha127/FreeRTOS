35aba9a957f63dcc8a1367506cfaf0f6625ee318
Vamshi Reddy <vamshikrish127@gmail.com>
2024-03-18 05:15:34 +0530

    added 15th module

---
+++ sycning_tasks_with_queues_15/Core/Src/main.c
@@ -0,0 +1,275 @@
+#include "main.h"
+#include "cmsis_os.h"
+#include <stdio.h>
+
+UART_HandleTypeDef huart2;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+int __io_putchar(int ch);
+
+TaskHandle_t sender_handle_1,sender_handle_2,receiver_handle;
+
+void vSenderTask1(void *pvParameters);
+void vSenderTask2(void *pvParameters);
+void vReceiverTask(void *pvParameters);
+
+QueueHandle_t yearQueue;
+
+int main(void)
+{
+
+  HAL_Init();// initialises hardware stuff.It also initialize the timer that we want to use for delay function
+
+  /* Configure the system clock */
+  SystemClock_Config(); // configures main system clock and other buses
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+
+  yearQueue = xQueueCreate(5,sizeof(int32_t));
+
+  //creating two sender tasks of same priority and one receiver task of higher priority
+  //Output of this experiment is sendertask1 and sendertask2 will send the queue one after the another as they have same priority
+  //sending is synchronised by assigning same priority
+
+  xTaskCreate(vSenderTask1,
+		  	  "Sender Task 1",
+			  100,
+			  NULL,
+			  1,
+			  &sender_handle_1);
+
+  xTaskCreate(vSenderTask2,
+		  	  "Sender Task 2",
+			  100,
+			  NULL,
+			  1,
+			  &sender_handle_2);
+
+  xTaskCreate(vReceiverTask,
+  		  	  "Receiver Task",
+  			  100,
+  			  NULL,
+  			  2,
+  			  &receiver_handle);
+
+  /* Init scheduler */
+  vTaskStartScheduler();
+
+  while(1)
+  {
+  }
+}
+
+void vSenderTask1(void *pvParameters)
+{
+	int32_t value_to_send = 2050;
+	BaseType_t queueStatus;
+
+	while(1)
+	{
+		queueStatus = xQueueSend(yearQueue,&value_to_send,0); //it return queueStatus of BaseType_t
+		//here block state wait time is 0. return from xQueueSend without any wait
+
+		if(queueStatus != pdPASS)//pdPASS means true
+		{
+			printf("Error: Data couldn't be sent .....\n\r");
+		}
+
+		for(int i=0;i< 10000; i++){}//here task wont be in the blocked/wait state
+	}
+}
+
+void vSenderTask2(void *pvParameters)
+{
+	int32_t value_to_send = 5050;
+	BaseType_t queueStatus;
+
+	while(1)
+	{
+		queueStatus = xQueueSend(yearQueue,&value_to_send,0); //it return queueStatus of BaseType_t
+		//here block state wait time is 0. return from xQueueSend without any wait
+
+		if(queueStatus != pdPASS)//pdPASS means true
+		{
+			printf("Error: Data couldn't be sent .....\n\r");
+		}
+
+		for(int i=0;i< 10000; i++){}//here task wont be in the blocked/wait state
+	}
+}
+
+void vReceiverTask(void *pvParameters)
+{
+	int32_t value_received;
+	const TickType_t wait_time = pdMS_TO_TICKS(100);//100ms
+	BaseType_t queueStatus;
+
+	while(1)
+	{
+		queueStatus = xQueueReceive(yearQueue,&value_received,wait_time);
+
+		if(queueStatus == pdPASS)
+		{
+			printf("Received Value :   %ld .... \n\r",value_received);
+		}
+		else
+		{
+			printf("Error: Data couldn't receive .....\n\r");
+		}
+	}
+}
+
+int uart2_write(int ch)
+{
+	while(!(USART2->SR & 0x0080)){} //checking the status register. Waiting here for the transmit buffer to be empty
+
+	USART2->DR	=  (ch & 0xFF); // only 8 bit data at once
+
+	return ch;
+}
+
+int __io_putchar(int ch)
+{
+	//HAL_UART_Transmit(&huart2,(uint8_t* )&ch, 1, 0xFFFF);
+	// HAL_UART_Transmit function is taking more time to print. so, we are going to make some light function to transmit
+
+	uart2_write(ch); // This is the new function
+	return ch;
+
+}
+
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
